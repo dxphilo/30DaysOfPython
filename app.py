04cd95d95b83947bcda17b0ba995794f750acea7
John Philip <developerphilo@gmail.com>
2021-09-13 12:10:13 -0700

    Day 2 of 30daysog pythong complete

--- app.py
+++ app.py
@@ -9,3 +9,86 @@
 # is_published = False
 # print(price + rating)
 
+
+# DAY TWO.
+
+# capture input from the user and print it back
+
+# name = input('please enter your name ')
+
+# print('Hi ' + name + " thanks for coming over here")
+
+# simple exercise ask two questions form the user and persona name and favourite colour and print them back.
+
+# name = input('What is your name ')
+# favColor = input('What is your favourite colors ')
+
+# print('Hi ' + name + " " + favColor + " if your favourite colour")
+# type of variables
+
+# birth_year = input('Enter your birth year ')
+# your_age = 2021 - int(birth_year)
+
+# print("You are " + str(your_age) + " years old")
+
+
+# program that asks that aske the users their weight and donverts it into kilograms
+# print the answer to the terminal.
+
+# user_weight = input('Enter your weight in pounds ')
+# weight = int(user_weight)
+
+# user_Kiloweight = weight *  0.453592
+
+# print(user_Kiloweight)
+
+# contatenations
+# course = 'Python for "beginners"'
+# print(course)
+
+# course = "Python for 'beginners'"
+# print(course)
+# strings tha expands many lines, can both be singel or double qoutes.
+
+# course = '''
+# Hi Mosh Hamedani,
+
+# Here is our initial email to you.
+
+
+# Thank you.
+
+# From the support Team
+
+# '''
+
+# print(course)
+
+# index of strings in python. - similar to indexing of arrays.
+
+# course = 'Python for Beginners'
+# print(course[0:3])
+# print(course[0:])
+# print(course[1:])
+# print(course[:5])
+
+# # can be used to clone a string
+# print(course[:])
+# retuns characters from index 0 -3 but excludes the number of index 3.
+
+# simple exercise.
+# name ='Jennifer'
+
+# print(name[1:-1])
+
+# first_name = 'John'
+# second_name = 'Philip'
+
+# print(first_name + " [" + second_name + " ] is a coder")
+
+# use of formatted strings to vosualize the output.
+# first_name = 'John'
+# second_name = 'Philip'
+
+# message = f'{first_name}  [{second_name}] is a coder'
+# print(message)
